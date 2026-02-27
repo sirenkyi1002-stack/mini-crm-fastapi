ff7733aba6abd5bf42c933d27485b2b338d3760e
Pasha <sirenkyi1002@gmail.com>
2026-02-27 23:14:58 +0200

    Add main.py with initial FastAPI endpoints

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Fake список клієнтів
+
 clients = [
     {"id": 1, "name": "Artem", "balance": 1000},
     {"id": 2, "name": "Olena", "balance": 2500},
     {"id": 3, "name": "Ivan", "balance": 500},
 ]
 
-# Endpoint /
+
 @app.get("/")
 def root():
-    return {"message": "Mini CRM працює 🚀"}
+    return {"message": "Mini CRM працює "}
 
-# Endpoint /clients
+
 @app.get("/clients")
 def get_clients():
     return clients
